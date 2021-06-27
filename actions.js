17d3d5f348a145d4d4a1fc452f691e72c9989f8c
Eliza Ahmed <elizarafee@gmail.com>
2021-06-27 11:18:52 +0100

    installed redux and created a action

---
+++ actions.js
@@ -0,0 +1,13 @@
+const redux = require('redux');
+
+
+// creating a constant for a action Buying Cake
+const BUY_CAKE = 'BUY_CAKE';
+
+// this function is a action Creator which returns a action
+function buyCake() {    // returning a function
+    return {    
+        type: BUY_CAKE,
+        info: 'First redux action'        
+    }
+}
